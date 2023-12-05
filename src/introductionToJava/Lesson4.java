eb2abff5fd7e6c0a07390d427e34df0e4852f701
Your <ngothuygiang.io@gmail.com>
2023-12-06 01:50:53 +0700

    introduction to java

---
+++ src/introductionToJava/Lesson4.java
@@ -0,0 +1,29 @@
+package introductionToJava;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+        long a = 10L;
+        int b = 100;
+
+        boolean x = true;
+        boolean y = false;
+
+//    So sánh
+        System.out.println("a == b " + (x || y));
+        System.out.println("!x = "+(!x));
+
+//        long x = ++a;
+//        long y = a++;
+
+//        System.out.println("a = " + a);
+//
+//        a += b;
+//        System.out.println("a = " + a);
+//
+////        long sum = a + b;
+//        System.out.println("++a = " + x);
+//
+//        System.out.println("a++ = " + y);
+////        System.out.println("a % b = " + (a % b));
+    }
+}
